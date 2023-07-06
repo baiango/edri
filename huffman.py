023622c3a46eebdc1bc6dced6161e65c4bd9ada4
baiango <105705580+baiango@users.noreply.github.com>
2023-07-06 14:36:18 +0800

    Update huffman.py

--- huffman.py
+++ huffman.py
@@ -1,3 +1,8 @@
+from icecream import ic
+ic.configureOutput(includeContext=True) # It include the printing command and the line
+print = ic()
+'pip3 install icecream'
+
 def hc_enc(input):
 	ret = ""
 	weights = [0] * 256
@@ -18,10 +23,10 @@ def hc_enc(input):
 				weights[i], weights[j] = weights[j], weights[i]
 				characters[i], characters[j] = characters[j], characters[i]
 
+	'ABCDDEDEEEEEEEFFFGHIJKK'
+	# print(characters)
 	characters = ['A', 'B', 'C', 'G', 'H', 'I', 'J', 'K', 'D', 'F', 'E']
-	print(characters)
-	print(weights)
-
+	# print(weights)
 
 	class Node:
 		def __init__(self, left, right, sum):
@@ -36,22 +41,51 @@ def hc_enc(input):
 	tree = None
 	for _ in [0]:
 		swap = []
-		weights_deduped = list(dict.fromkeys(weights))
+		weights_deduped = []
+		for i in range(len(weights) - 1): # list(dict.fromkeys(weights)) replacement
+			if weights[i] is not weights[i + 1]:
+				weights_deduped.append(weights[i])
+		weights_deduped.append(weights[-1])
+
+		# count = weights.count(weights_deduped[0])
+		# offset = weights.index(weights_deduped[0])
+		# for i in range(0, count - 1, 2):
+		# 	swap.append(Node(characters[i], characters[i + 1], weights[i] + weights[i + 1]))
+		# for i in range(0, len(swap) - 1, 2):
+		# 	if swap[i].sum <= swap[i + 1].sum:
+		# 		swap[i] = Node(swap[i], swap[i + 1], swap[i].sum + swap[i + 1].sum)
+		# 		swap.pop(1)
+		# if count & 1:
+		# 	swap[-1] = Node(characters[offset], swap[-1], weights[offset] + swap[-1].sum)
+
 		for iter in weights_deduped:
 			count = weights.count(iter)
 			offset = weights.index(iter)
 			# Merge double
-			for i in range(offset, offset + count - 1, 2):
+			print(swap)
+			for i in range(offset, offset + count - 2, 2):
 				swap.append(Node(characters[i], characters[i + 1], weights[i] + weights[i + 1]))
 			# Merge 2 doubles
-			for i in range(0, len(swap) - 1, 2):
+			print(swap)
+			for i in range(0, len(swap) - 2, 2):
 				if swap[i].sum <= swap[i + 1].sum:
 					swap[i] = Node(swap[i], swap[i + 1], swap[i].sum + swap[i + 1].sum)
 					swap.pop(1)
 			# Merge single
-			if count & 1:
-				swap[-1] = Node(characters[offset], swap[-1], weights[offset] + swap[-1].sum)
-		tree = Node(swap[0], swap[1], swap[0].sum + swap[1].sum)
+			print(swap)
+			if count != 1 and count & 1:
+				swap[-1] = Node(characters[offset + count - 1], swap[-1], weights[offset + count - 1] + swap[-1].sum)
+			# for i in range(0, len(swap) - 1, 2):
+			# 	if swap[i].sum >= swap[i + 1].sum:
+			# 		swap[i] = Node(swap[i + 1], swap[i], swap[i].sum + swap[i + 1].sum)
+			# 		swap.pop(1)
+			print(swap)
+			if iter == weights_deduped[1]:
+				pass
+				# break
+		# print(swap)
+		swap = Node(swap[0], swap[1], swap[0].sum + swap[1].sum)
+		tree = swap
 
 	# Path generator (Breadth-first search)
 	path = []
@@ -68,10 +102,14 @@ def hc_enc(input):
 			queue.pop(0)
 			pos.pop(0)
 
+	# print(tree.right.left)
+
 	print(tree)
-	print(weights)
-	print(path)
-	print(characters)
+	print(len(weights))
+	# print(len(path))
+	# print(path)
+	# print(weights)
+	# print(characters)
 	return ret
 
 
